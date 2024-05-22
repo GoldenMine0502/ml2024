75f1ef98df0bffbda16ee50b9709ca3119e3afe3
GoldenMine0502 <ehe123@naver.com>
2024-05-23 01:48:19 +0900

    ㅋ

--- run_estimate.sh
+++ run_estimate.sh
@@ -4,7 +4,7 @@ model_type=DCUNET
 dataset_type=VCTK16K
 epoch=50
 output_data_dir=estimate/${model_type}/
-chkpt_path=chkpt/${model_type}/chkpt_30.pt
+chkpt_path=chkpt/${model_type}_28k/chkpt_50.pt
 
 train=0
 inference=2
@@ -19,3 +19,7 @@ python3 trainer.py \
   -pp $chkpt_path\
   --train $train \
   --inference $inference
+
+
+#     train_folder = '/mnt/nvme/dataset/wav16k/train/'
+  #    test_folder = '/mnt/nvme/dataset/wav16k/test/'
